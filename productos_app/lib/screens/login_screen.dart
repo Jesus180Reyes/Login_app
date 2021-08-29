cb2d794128d39690c2f1d951ea4ae6e5494cdefa
Jesus180Reyes <luisdejesus200122@gmail.com>
2021-08-29 00:33:28 -0600

    Nueva Actualizacion

--- productos_app/lib/screens/login_screen.dart
+++ productos_app/lib/screens/login_screen.dart
@@ -1,11 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/auth_background.dart';
+import 'package:productos_app/widgets/card_container.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AuthBackground(),
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 250,
+              ),
+              CardContainer(
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Text(
+                      'Login',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    SizedBox(height: 39),
+                    LoginForm(),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+              Text(
+                'Crear Una Nueva Cuenta',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'john@example.com',
+                labelText: 'Correo Electronico',
+                prefixIcon: Icons.alternate_email_rounded,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              obscureText: true,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '******',
+                labelText: 'Contrasena',
+                prefixIcon: Icons.lock_outline,
+              ),
+            ),
+            SizedBox(height: 30),
+            MaterialButton(
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 80,
+                  vertical: 15,
+                ),
+                child: Text(
+                  'Ingresar:',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- productos_app/lib/widgets/auth_background.dart
+++ productos_app/lib/widgets/auth_background.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AuthBackground extends StatelessWidget {
+  final Widget child;
+
+  const AuthBackground({Key? key, required this.child}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -10,7 +13,26 @@ class AuthBackground extends StatelessWidget {
       child: Stack(
         children: [
           _PurpleBox(),
+          _HeaderIcon(),
+          child,
         ],
+      ),
+    );
+  }
+}
+
+class _HeaderIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 30),
+        width: double.infinity,
+        child: Icon(
+          Icons.person_pin,
+          color: Colors.white,
+          size: 100,
+        ),
       ),
     );
   }
